8e5413c9fcbcb51ed8279e216ec9d79f26e339c0
DTx-BRibeiro <bruno.ribeiro@dtx-colab.pt>
2022-03-07 15:48:56 +0000

    module 2 update

--- ssg/site.py
+++ ssg/site.py
@@ -3,10 +3,11 @@ import os
 
 class Site():
 
-    def __init__(self, source: str, dest: str):
+    def __init__(self, source: str, dest: str, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
-        return 
+
+        self.parsers = parsers or [] 
 
     def create_dir(self, path: Path):
         directory = self.dest/path.relative_to(self.source)
@@ -18,5 +19,20 @@ class Site():
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
             else:
                 continue
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+    
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
